d6619bdb513e6ae9c2437a2a1f41af9bb3b02f97
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2023-11-29 16:50:51 +0100

    pulizia u.u

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,7 @@ import es_1.Rettangolo;
 public class Main {
     public static void main(String[] args){
          Rettangolo area=new Rettangolo();
-         System.out.println(area.getAltezza());
-         System.out.println(area.getLarghezza());
-        System.out.println(area.getLar());
-        System.out.println(area.getAlt());
+
 
           area.setAltezza(23);
           area.setLarghezza(36);
